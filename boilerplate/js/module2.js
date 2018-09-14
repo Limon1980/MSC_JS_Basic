c8b63dd71bd07a89ebd07f50c4542b47a0f26151
Limon1980 <tarkalmob@yandex.ru>
2018-09-14 15:47:22 +1000

    aple many script

--- boilerplate/js/module2.js
+++ boilerplate/js/module2.js
@@ -1,3 +1,5 @@
+document.body.innerHTML = prompt('Введите любой html-код в системное окно')
+
 var many = parseFloat(prompt('сколько у вас с собой есть денег?'))
 var aple = parseFloat(prompt('сколько кг вы купили яблок?'))
 var baton = parseFloat(prompt('сколько вы купили батонов хлеба?'))
