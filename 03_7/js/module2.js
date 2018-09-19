8eb4a207fb4fc742c1c3c28f9e65df95dbe00d2c
Limon1980 <tarkalmob@yandex.ru>
2018-09-19 16:17:32 +1000

    animated ball

---
+++ 03_7/js/module2.js
@@ -0,0 +1,8 @@
+var many = parseFloat(prompt('сколько у вас с собой есть денег?'));
+var aple = parseFloat(prompt('сколько кг вы купили яблок?'));
+var baton = parseFloat(prompt('сколько вы купили батонов хлеба?'));
+var all_aple = aple*parseFloat(prompt('сколько   стоит один кг яблок  ?'));
+var all_baton = baton*parseFloat(prompt('сколько стоит один батон ?'));
+many = many - (all_aple+all_baton);
+var raz = many > 0;
+document.body.innerHTML = 'Денег хватило '+raz+ '<br>Осталось денег '+many;
