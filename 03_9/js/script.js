6fc79ace4983e790a7e2b5b66c4852678b9ccaa9
Limon1980 <tarkalmob@yandex.ru>
2018-09-20 13:03:21 +1000

    dz 03_9 full

--- 03_9/js/script.js
+++ 03_9/js/script.js
@@ -1,11 +1,31 @@
 console.log('You are at '+window.location);
 
 const body = document.querySelector('body');
-console.log(body);
-console.log(body.hasAttribute('background-color'));
+console.log(body.hasAttribute('bgcolor'));
+
+const page = document.querySelector('.page');
+console.log(page);
+
+const name = document.querySelector('span.name');
+console.log(name);
+
+const image = document.querySelector('img');
+console.log(image);
+
+const shortBio = document.querySelector('.shortBio');
+console.log(shortBio);
 
 let questcolorboby = prompt('Какой ты хочешь цвет boby?');
-body.setAttribute('background-color', questcolorboby);
+body.setAttribute('bgcolor', questcolorboby);
 
 let questcolortext = prompt('Какой ты хочешь цвет текста?');
-body.setAttribute('color', questcolortext);
+page.style['color'] = questcolortext;
+
+let questname = prompt('Как вас зовут?');
+name.innerHTML = questname;
+
+let questimage = prompt('Адрес картинки?');
+image.setAttribute('src', questimage);
+
+let shortBioText = prompt('Текст shortBio?');
+shortBio.innerHTML = shortBioText;
